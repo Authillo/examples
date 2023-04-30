04b76e31f40ffe5089b70b801ff46a4e0e3b0374
James Grom <jgrom@scu.edu>
2023-04-30 14:25:58 -0700

    install authillo backend-js

--- backends/express/src/index.ts
+++ backends/express/src/index.ts
@@ -1,6 +1,9 @@
+import { Authillo } from "@authillo/backend-js";
 const express = require("express");
 const app = express();
 const port = 3000;
+
+Authillo.initialize({ clientId: "myClientId", clientSecret: "myClientSecret", jwtKey: "myJwtKey" });
 
 app.get("/hello", (req: any, res: any) => {
 	console.log("hello enpoint hit");
