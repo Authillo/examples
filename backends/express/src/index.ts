8a112009d834455b5f0e63a47c9931dd2fe230ec
James Grom <jgrom@scu.edu>
2023-04-30 14:18:00 -0700

    configure basic express usage

--- backends/express/src/index.ts
+++ backends/express/src/index.ts
@@ -1,62 +1,24 @@
-// const express = require("express");
-// const fetch = import("node-fetch");
+const express = require("express");
+import * as fetch from "node-fetch";
+import * as bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+app.use(
+	(
+		req: any,
+		res: { setHeader: (arg0: string, arg1: string | boolean) => void },
+		next: () => void
+	) => {
+		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+		res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+		res.setHeader("Access-Control-Allow-Credentials", true);
+		next();
+	}
+);
 
-// // import * as fetch from "node-fetch";
-// import { authillo } from "@authillo/verify";
+app.use(bodyParser.json());
 
-// // Set up the API endpoint URL
-// const userId = "W1RU9Mv7pVSd13jCaIPWhQGnHzyWl6jmy6zRtbhs"; // Replace with the ID of the user you want to retrieve enrollments for
-// const url = `https://camino.instructure.com/api/v1/users/${userId}/enrollments`;
-
-// // Set up the API request headers
-// const accessToken = "1788~vCmIimTi81YsltNOin9jt4sG7rgAqlJuogUnrlCw7oGxITj0ure3MRjK5DfyMk6t"; // Replace with your Canvas API access token
-// const headers = {
-// 	Authorization: `Bearer ${accessToken}`,
-// };
-
-// const app = express();
-// const port = 3000;
-
-// app.get("/camino", async (req, res) => {
-// 	// Make the API request using fetch()
-// 	const resy = await fetch(url, {
-// 		method: "GET",
-// 		headers: headers,
-// 	})
-// 		.then((response) => {
-// 			if (response.ok) {
-// 				return response.json();
-// 			} else {
-// 				throw new Error("Error retrieving enrollments");
-// 			}
-// 		})
-// 		.then((data) => {
-// 			// Do something with the enrollment data
-// 			console.log(data);
-// 		})
-// 		.catch((error) => {
-// 			// Handle any errors that occur during the API request
-// 			console.error(error);
-// 		});
-// 	// const verificationStatus = await authilloClient.verify("+19145841378", ["face", "email"]);
-// 	res.send(resy);
-// 	// res.send(verificationStatus);
-// });
-// // define a route handler for the default home page
-// // initialze your app
-// const authilloClient = new authillo({
-// 	clientId: "myApp's Client Id",
-// 	clientSecret: " myApp's Client Secret",
-// });
-// app.get("/verify", async (req, res) => {
-// 	const verificationStatus = await authilloClient.verify("+19145841378", ["face", "email"]);
-// 	res.send(verificationStatus);
-// });
-// app.get("/info", async (req, res) => {
-// 	const verifiedInfo = await authilloClient.getVerifiedInfo("+19145841378", ["face", "email"]);
-// 	res.send(verifiedInfo);
-// });
-// // start the Express server
-// app.listen(port, () => {
-// 	console.log(`server started at http://localhost:${port}`);
-// });
+app.get("/hello", (req: any, res: any) => {
+	console.log("hello enpoint hit");
+});
