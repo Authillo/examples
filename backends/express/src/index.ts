89ad03ff859c3d500ad4adf5b59735d786a6cc81
James Grom <jgrom@scu.edu>
2023-04-30 14:22:44 -0700

    basic configuration of express backend

--- backends/express/src/index.ts
+++ backends/express/src/index.ts
@@ -1,24 +1,10 @@
 const express = require("express");
-import * as fetch from "node-fetch";
-import * as bodyParser from "body-parser";
 const app = express();
 const port = 3000;
-app.use(
-	(
-		req: any,
-		res: { setHeader: (arg0: string, arg1: string | boolean) => void },
-		next: () => void
-	) => {
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-		res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-		res.setHeader("Access-Control-Allow-Credentials", true);
-		next();
-	}
-);
-
-app.use(bodyParser.json());
 
 app.get("/hello", (req: any, res: any) => {
 	console.log("hello enpoint hit");
+});
+app.listen(port, () => {
+	console.log(`express server started listening on ${port}`);
 });
