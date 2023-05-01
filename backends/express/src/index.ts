a072c5251f29ff5842ba90f1e03ca5b846b05673
James Grom <jgrom@scu.edu>
2023-04-30 17:28:59 -0700

    refactor to use esm based express

--- backends/express/src/index.ts
+++ backends/express/src/index.ts
@@ -1,24 +1,18 @@
 import { Authillo } from "@authillo/backend-js";
 import * as bodyParser from "body-parser";
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
 
 Authillo.initialize({ clientId: "myClientId", clientSecret: "myClientSecret", jwtKey: "myJwtKey" });
 
-app.use(
-	(
-		req: any,
-		res: { setHeader: (arg0: string, arg1: string | boolean) => void },
-		next: () => void
-	) => {
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-		res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-		res.setHeader("Access-Control-Allow-Credentials", true);
-		next();
-	}
-);
+app.use((req, res, next): void => {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	next();
+});
 app.use(bodyParser.json());
 app.get("/hello", (req: any, res: any) => {
 	console.log("hello enpoint hit");
