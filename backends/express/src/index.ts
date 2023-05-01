926fc0d5c3db01b44bd2e62dbaf4d0f6249f8482
James Grom <jgrom@scu.edu>
2023-04-30 18:02:57 -0700

    cursory implementation of express backend

--- backends/express/src/index.ts
+++ backends/express/src/index.ts
@@ -1,13 +1,7 @@
-import { Authillo } from "@authillo/backend-js";
 import * as bodyParser from "body-parser";
 import express from "express";
 const app = express();
 const port = 3000;
-
-let codeVerifier;
-
-Authillo.initialize({ clientId: "myClientId", clientSecret: "myClientSecret", jwtKey: "myJwtKey" });
-
 app.use((req, res, next): void => {
 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
@@ -16,27 +10,31 @@ app.use((req, res, next): void => {
 	next();
 });
 app.use(bodyParser.json());
-app.get("/hello", (req: any, res: any) => {
-	console.log("hello enpoint hit");
-});
-
-/**
- * GENERATE CODE CHALLENGE STEP -
- * 1) Randomly generate a codeVerifier string
- * 2) Generate the codeChallenge by setting it equal to the hash of the codeVerifier using SHA256
- * 3) Store both using a persistent backend database
- * 4) Send the user the hashed version of the codeVerifier ( aka the codeChallenge)
- */
-app.get("/codechallenge", (req, res) => {
-	const codePair = Authillo.generateCodeChallengeAndCodeVerifierPair();
-	console.log(codePair);
-	/**
-	 * WARNING - This is an example backend so we don't store these codes using a database & we instead save the values in the codeVerifier & codeChallenge variables respectively.
-	 * In production, your backend would have to store the codeVerifier & codeChallenge in a presistent database
-	 */
-	codeVerifier = codePair.codeVerifier;
-	res.json(`${codeVerifier}`);
-});
 app.listen(port, () => {
 	console.log(`express server started listening on ${port}`);
+});
+
+// ^^^^^^ EVERYTHING ABOVE THIS LINE IS BOILERPLATE EXPRESS CODE ^^^^^^^
+
+// vvvvvvv AUTHILLO SPECIFIC CODE BELOW vvvvvvv
+import { Authillo } from "@authillo/backend-js";
+Authillo.initialize({ clientId: "myClientId", clientSecret: "myClientSecret", jwtKey: "myJwtKey" });
+let codeVerifierStoredInMockDatabase: string;
+
+// GENERATE CODE CHALLENGE STEP
+app.get("/codechallenge", (req, res) => {
+	const { codeVerifier, codeChallenge } = Authillo.generateCodeChallengeAndCodeVerifierPair();
+	codeVerifierStoredInMockDatabase = codeVerifier;
+	res.json(`${codeChallenge}`);
+});
+
+// TOKEN REQUEST & USER INFO STEP
+app.get("/codeResponse", async (req, res) => {
+	const code = req.query.code as string;
+	const tokenResponse = await Authillo.tokenRequest(code, "", codeVerifierStoredInMockDatabase);
+	if (tokenResponse.result.succeeded) {
+		const userInfo = await Authillo.getUserInfo(tokenResponse.result.feedback.access_token);
+		console.log("retrieved the following info about the user", userInfo);
+	}
+	res.json(JSON.stringify(tokenResponse));
 });
