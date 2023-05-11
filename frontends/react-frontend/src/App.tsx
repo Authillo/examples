9a15f1876612377369b6f3bf5c9673852e25f301
James Grom <jgrom@scu.edu>
2023-05-11 16:26:04 -0700

    remove unecessary files from react app

--- frontends/react-frontend/src/App.tsx
+++ frontends/react-frontend/src/App.tsx
@@ -1,32 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { Authillo } from "@authillo/frontend-js";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<button
-					onClick={() => {
-						Authillo.begin(["face"]);
-					}}
-				>
-					Login With Authillo
-				</button>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<button
+				onClick={() => {
+					Authillo.begin(["face"]);
+				}}
+			>
+				Login or Verify With Authillo
+			</button>
 		</div>
 	);
 }
